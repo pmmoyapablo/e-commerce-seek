2d5bf617f8d5c88d2a5a28d8e6928a08abdb9ae4
Pablo Moya <pmmoyapablo@gmail.com>
2025-04-28 11:20:49 -0500

    Update model User

--- mcs_users-service/src/main/java/com/seek/users/domain/model/User.java
+++ mcs_users-service/src/main/java/com/seek/users/domain/model/User.java
@@ -4,37 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Date;
-import com.vectora.transactionservice.domain.exception.UserNotFoundException;
+import java.util.UUID;
+
+import com.seek.users.domain.exception.UserNotFoundException;
 
 @Data // Lombok: getters, setters, toString, equals, hashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private Long id;
-    private Long fromAccount;
-    private Long toAccount;
-    private double monto;
-    private Date fecha;
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
 
     // Constructor sin ID para creación
-    public User(Long fromAccount, Long toAccount, double monto) {
-        this.fromAccount = fromAccount;
-        this.toAccount = toAccount;
-        this.monto = monto;
-        this.fecha = new Date();
+    public User() {
     }
 
-    public boolean validateAmount() {
-        return this.monto > 0;
-    }
-
-    public void validateAccounts() {
-        if (!(this.fromAccount != null && this.toAccount != null)) {
-            throw new UserNotFoundException("Accounts are required");
-        }
-
-        if (this.fromAccount.equals(this.toAccount)) {
-            throw new UserNotFoundException("Accounts cannot be the same");
-        }
-    }
 }
